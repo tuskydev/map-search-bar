2192a0119efec39118bc8a60041ce8d41d0905d0
Mario Fernandez <diddleslip@gmail.com>
2020-09-15 08:19:13 -0400

    Figured out the schematics for MapBox GL, also added track-location, and search bar components to map

--- search-bar/src/components/Map.js
+++ search-bar/src/components/Map.js
@@ -1,11 +1,50 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+import './Map.css';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
-function Map() {
+mapboxgl.accessToken = 'pk.eyJ1IjoibGFtYmRhbGFiczI1ZWNvc29hcCIsImEiOiJja2VhZWRhOG4wNmU5MnNxZXQ0bmhxZnU3In0.zWyuwunBSy51dulZG9gowQ';
+
+function MapMap() {
+  useEffect(() => {
+    var map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/lambdalabs25ecosoap/ckeaib2n30b4f19mq6mj2dsq3', // stylesheet location
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9 // starting zoom
+    });
+
+    map.addControl(
+      new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl
+      })
+    );
+
+    var geocoder = new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken,
+      types: 'country,region,place,postcode,locality,neighborhood'
+    });
+       
+    geocoder.addTo('.geocoder');
+
+    map.addControl(
+      new mapboxgl.GeolocateControl({
+      positionOptions: {
+      enableHighAccuracy: true
+      },
+      trackUserLocation: true
+      })
+    );
+  }, []);
+
   return (
-    <div>
-      <h2>Hello from Map.js</h2>
-    </div>
+    <section>
+      <div className="geocoder"/>
+      <div id="map"/>
+    </section>
   )
 }
 
-export default Map
+export default MapMap;
